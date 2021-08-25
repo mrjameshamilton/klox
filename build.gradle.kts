926da7daf753911c00bafbc57069816674d775ed
James Hamilton <mrjameshamilton@gmail.com>
2021-08-25 21:02:50 +0200

    Update test dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
 
-    testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.6.0")
-    testImplementation("io.kotest:kotest-property-jvm:4.6.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.21")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.6.1")
+    testImplementation("io.kotest:kotest-property-jvm:4.6.1")
     testImplementation("io.mockk:mockk:1.12.0")
 }
 
