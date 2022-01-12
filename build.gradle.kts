07feb425d46a52d3c1e2c5b9ed5d646f7e092f20
James Hamilton <mrjameshamilton@gmail.com>
2022-01-12 22:09:20 +0100

    Update ktlint to version 10.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     application
     jacoco
 }
